64ea64686bc667221cfeac9d8c0089aca6cef683
SquiddyPoos <liuwenkai2017@gmail.com>
2023-09-07 23:00:30 +0800

    remove unused import

--- frontend/src/HomeBase.tsx
+++ frontend/src/HomeBase.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Spacer, VStack } from "@chakra-ui/react"
+import { Box, Flex, Heading, Spacer } from "@chakra-ui/react"
 import { NavLink, Outlet } from "react-router-dom"
 import { homeRoutes } from "./routes"
 import NavBox from "./NavBox"
